cf2fe162d2dcb90e6ab9e50436287ba442655e3e
Deivz <carlosd_oliveira@hotmail.com>
2022-07-22 01:32:05 -0300

    Pets são inseridos vindos de uma API

--- src/assets/pages/Home/index.jsx
+++ src/assets/pages/Home/index.jsx
@@ -1,16 +1,15 @@
 import Cards from '../../components/Cards';
 import './home.css';
-import dunga from '../../images/dunga.svg';
-import felicia from '../../images/felicia.svg';
-import sirius from '../../images/sirius.svg';
-import fiona from '../../images/fiona.svg';
-import sid from '../../images/sid.svg';
-import yoda from '../../images/yoda.svg';
-import lua from '../../images/lua.svg';
-import amora from '../../images/amora.svg';
-import zelda from '../../images/zelda.svg';
+import { api } from '../../../services/api';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+    const [pets, setPets] = useState([]);
+    useEffect(() => {
+        api.get('/pets').then((response) => {
+            setPets(response.data);
+        })
+    }, []);
 
     return (
         <section className='secao__home'>
@@ -20,78 +19,17 @@ export default function Home() {
                 <h2 className='home__titulo'>Veja os amigos disponíveis para adoção!</h2>
             </div>
             <div className='home__cards'>
-                <Cards
-                    imagem={dunga}
-                    nome={'Dunga'}
-                    idade={'2 anos'}
-                    porte={'Porte pequeno'}
-                    perfil={'Calmo e educado'}
-                    cidade={'Rio de Janeiro (RJ)'}
-                />
-                <Cards
-                    imagem={felicia}
-                    nome={'Felicia'}
-                    idade={'3 meses'}
-                    porte={'Porte pequeno'}
-                    perfil={'Ativa e carinhosa'}
-                    cidade={'Nova Iguaçu (RJ)'}
-                />
-                <Cards
-                    imagem={sirius}
-                    nome={'Sirius'}
-                    idade={'6 meses'}
-                    porte={'Porte grande'}
-                    perfil={'Ativo e educado'}
-                    cidade={'Duque de Caxias (RJ)'}
-                />
-                <Cards
-                    imagem={fiona}
-                    nome={'Fiona'}
-                    idade={'3 anos'}
-                    porte={'Porte pequeno'}
-                    perfil={'Calma e carinhosa'}
-                    cidade={'São Gonçalo (RJ)'}
-                />
-                <Cards
-                    imagem={sid}
-                    nome={'Sid'}
-                    idade={'8 meses'}
-                    porte={'Porte médio/grande'}
-                    perfil={'Brincalhão e amável'}
-                    cidade={'Rio de Janeiro (RJ)'}
-                />
-                <Cards
-                    imagem={yoda}
-                    nome={'Yoda'}
-                    idade={'1 ano'}
-                    porte={'Porte médio'}
-                    perfil={'Ativo e carinhoso'}
-                    cidade={'Nova Iguaçu(RJ)'}
-                />
-                <Cards
-                    imagem={lua}
-                    nome={'Lua'}
-                    idade={'6 meses'}
-                    porte={'Porte médio'}
-                    perfil={'Ativa e carinhosa'}
-                    cidade={'Duque de caxias (RJ)'}
-                />
-                <Cards
-                    imagem={amora}
-                    nome={'Amora'}
-                    idade={'45 dias'}
-                    porte={'Porte grande'}
-                    perfil={'Ativa e carinhosa'}
-                    cidade={'São Gonçalo (RJ)'}
-                />
-                <Cards
-                    imagem={zelda}
-                    nome={'Zelda'}
-                    idade={'5 meses'}
-                    porte={'Porte médio'}
-                    perfil={'Ativa e amável'}
-                    cidade={'Rio de Janeiro (RJ)'}
-                />
+                {pets.map((pet) => {
+                    return <Cards
+                        key={pet.id}
+                        imagem={pet.imagem}
+                        nome={pet.nome}
+                        idade={pet.idade}
+                        porte={pet.porte}
+                        perfil={pet.perfil}
+                        cidade={pet.local}
+                    />
+                })}
             </div>
         </section>
     );
