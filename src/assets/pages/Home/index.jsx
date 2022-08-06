a09c102b1a847b6356781d90d263808b41b90771
Deivz <carlosd_oliveira@hotmail.com>
2022-08-05 22:57:56 -0300

    Resolvido bug da função maps na pagina de pets

--- src/assets/pages/Home/index.jsx
+++ src/assets/pages/Home/index.jsx
@@ -5,14 +5,12 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
     const [pets, setPets] = useState([]);
-    // useEffect(() => {
-    //     api.get('/pets').then((response) => {
-    //         setPets(response.data);
-    //     })
-    // }, []);
-    api.get('/pets').then((response) => {
-        setPets(response.data);
-    });
+    useEffect(() => {
+        api.get('/pets').then((response) => {
+            setPets(response.data);
+        })
+    }, []);
+
     return (
         <section className='secao__home'>
             <h2 className='home__titulo--mobile'>Olá! Veja os amigos disponíveis para adoção!</h2>
@@ -21,7 +19,7 @@ export default function Home() {
                 <h2 className='home__titulo'>Veja os amigos disponíveis para adoção!</h2>
             </div>
             <div className='home__cards'>
-                {pets.map((pet) => {
+                {Object.values(pets).map((pet) => {
                     return <Cards
                         key={pet.id}
                         imagem={pet.imagem}
